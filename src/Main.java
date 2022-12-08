2d2a154cd896b8c8a7e8fe02d2b2405e6255f89f
yank5 <yank5@nycstudents.net>
2022-12-08 11:38:11 -0500

    22

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,10 @@
 import sun.misc.Unsafe;
 public class Main {
-    public static void main(String[] args) {
-        final Unsafe unsafe = Unsafe.getUnsafe();
+    static final Unsafe unsafe = Unsafe.getUnsafe();
+    public static void test() {
         unsafe.setMemory(100, 100, (byte) 0x0000);
+    }
+    public static void main(String[] args) {
+        test();
     }
 }
